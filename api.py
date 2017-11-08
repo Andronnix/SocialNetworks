485ce38378dff386d5f27eb647add899c4512d73
Andronnix <w4UvOQY4LvLa>
2017-11-08 09:15:42 +0300

    added subscription loading + too much rps error fixed

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import json
+import time
 from collections import namedtuple
 from urllib.request import urlopen
 from urllib.parse import urlencode
@@ -12,8 +13,20 @@ def _build_url(method_name, params):
     params["v"] = 5.69
     params = urlencode(params)
 
-
     return "https://api.vk.com/method/{}?{}".format(method_name, params)
+
+
+def _retry_request(url):
+    retries = 10
+    while retries:
+        response = json.loads(urlopen(url).read().decode('utf8'))
+        if not ("error" in response and response["error"]["error_code"] == 6):
+            return response
+
+        time.sleep(0.1 * (11 - retries))
+        retries -= 1
+
+    raise IOError("Can't get data from vk, {}".format(response))
 
 
 def get_friends(user_id=None, list_id=None, fields=False):
@@ -33,10 +46,11 @@ def get_friends(user_id=None, list_id=None, fields=False):
 
     url = _build_url("friends.get", params)
 
-    response = json.loads(urlopen(url).read().decode('utf8'))["response"]["items"]
+    response = _retry_request(url)
+    response = response.get("response", dict()).get("items", [])
 
     if fields:
-        response = list(map(lambda x: User(x["id"], x["first_name"], x["last_name"]), response))
+        response = list(map(lambda x: User.create(x["id"], x["first_name"], x["last_name"]), response))
     else:
         response = list(map(User, response))
 
@@ -54,8 +68,26 @@ def get_friends_lists(user_id=None):
 
     url = _build_url("friends.getLists", params)
 
-    response = json.loads(urlopen(url).read().decode('utf8'))
-    response = response["response"]["items"]
+    response = _retry_request(url)
+    response = response.get("response", dict()).get("items", [])
+
+    return response
+
+
+def get_subscriptions(user_id=None):
+    if user_id is None:
+        user_id = CONFIG["USER_ID"]
+
+    params = dict(
+        user_id=user_id,
+        extended=0,
+        count=200
+    )
+
+    url = _build_url("users.getSubscriptions", params)
+
+    response = _retry_request(url)
+    response = response.get("response", dict()).get("groups", dict()).get("items", [])
 
     return response
 
@@ -68,18 +100,19 @@ class User:
 
     @staticmethod
     def me():
-        return User(CONFIG["USER_ID"])
+        return User(CONFIG["USER_ID"], 'Андрей', 'Кокорев')
 
-    def __new__(cls, *args, **kwargs):
-        user_id = args[0]
-        return User._users.setdefault(user_id, super().__new__(cls))
+    @staticmethod
+    def create(user_id, first_name=None, last_name=None):
+        return User._users.setdefault(user_id, User(user_id, first_name, last_name))
 
     def __init__(self, user_id, first_name=None, last_name=None):
         self.id = user_id
-        self._first_name = first_name
-        self._last_name = last_name
+        self.first_name = first_name
+        self.last_name = last_name
         self._friends = None
         self._lists = None
+        self._subscriptions = None
 
     def _get_friends(self):
         if self._friends is None:
@@ -100,6 +133,15 @@ class User:
 
         return self._lists
 
+    def _get_subscriptions(self) -> [int]:
+        if self._subscriptions is None:
+            self._subscriptions = get_subscriptions(self.id)
+
+        return self._subscriptions
+
+    def _get_name(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
     def __eq__(self, other):
         if isinstance(other, self.__class__):
             return self.id == other.id
@@ -110,8 +152,10 @@ class User:
         return hash(self.id)
 
     def __str__(self):
-        return str(self.__dict__)
+        return "U(id={}, name={} {})".format(self.id, self.first_name, self.last_name)
 
     friends = property(_get_friends)
     friend_lists = property(_get_lists)
+    name = property(_get_name)
+    subscriptions = property(_get_subscriptions)
 
--- task.py
+++ task.py
@@ -1,13 +1,63 @@
 from api import User
+import networkx as nx
+import matplotlib.pyplot as plt
 
-TISCHENKO = User(48075937)
-OVSIANNIKOVA = User(25445578)
-VESELKOVA = User(134158269)
+
+def calculate_multiplexity(u1, u2):
+    s1 = set(u1.subscriptions)
+    s2 = set(u2.subscriptions)
+    s = s1.intersection(s2)
+    return 1 + len(s)
+
+
 ME = User.me()
 
-template = "{:>5}, {:>10}, {:>10}, {:>10}"
+social = nx.Graph()
+social.add_nodes_from(ME.friends)
 
-print(template.format("", "Tischenko", "Ovsiannikova", "Veselkova"))
+labels = dict()
+labels[ME] = ME.name
 
-for lst in ME.friend_lists:
-    print(template.format(lst.name, TISCHENKO in lst.users, OVSIANNIKOVA in lst.users, VESELKOVA in lst.users))
+multiplexity = dict()
+max_multiplexity = -1
+max_multiplexity_friend = None
+for friend in ME.friends:
+    multiplexity[friend] = calculate_multiplexity(ME, friend)
+    labels[friend] = "{}, {}".format(friend.name, multiplexity[friend])
+    print(labels[friend])
+
+    if multiplexity[friend] > max_multiplexity:
+        max_multiplexity = multiplexity[friend]
+        max_multiplexity_friend = friend
+
+    social.add_edge(ME, friend, weight=multiplexity[friend], color='limegreen')
+    for friend2 in friend.friends:
+        if friend2 in ME.friends:
+            social.add_edge(friend2, friend, weight=calculate_multiplexity(friend2, friend), color='lightgray')
+
+print("My maximal multiplexity = {}, with {}".format(max_multiplexity, max_multiplexity_friend.name))
+
+plt.subplot(111)
+edges = social.edges()
+ecolors = [social[u][v]['color'] for u, v in edges]
+weights = [social[u][v]['weight'] for u, v in edges]
+
+nodes = social.nodes()
+nsizes = [(200 if user == ME else 10*multiplexity[user]) for user in nodes]
+ncolors = [('red' if user == ME else 'gray') for user in nodes]
+
+nx.draw(social,
+        pos=nx.spring_layout(social, weight='weight'),
+        edges=edges,
+        nodes=nodes,
+        # node_color=ncolors,
+        node_size=nsizes,
+        width=weights,
+        edge_color=ecolors,
+        font_size=8,
+        labels=labels,
+        figsize=(20, 10),
+        dpi=200
+)
+plt.show()
+
